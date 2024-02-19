ae86808edba661c94c259d5105a402fc2e793eac
Oleh Hurmanchuk <gyrmanchyk230506@gmail.com>
2024-02-19 20:48:14 +0200

    add basic layout

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,19 @@
+<footer>
+  Footer content
+</footer>
+
+<style>
+  footer {
+    position: absolute;
+    display: block;
+    bottom: 0;
+    left: 0;
+    box-sizing: border-box;
+    min-height: fit-content;
+    width: 100%;
+    padding: 20px;
+
+    color: var(--white);
+    background-color: var(--very-dark-violet);
+  }
+</style>
---
+++ src/lib/Header.svelte
@@ -0,0 +1,15 @@
+<header>
+  Header content
+</header>
+
+<style>
+  header {
+    display: block;
+    position: fixed;
+    box-sizing: border-box;
+    top: 0;
+    left: 0;
+    width: 100%;
+    padding: 30px;
+  }
+</style>
