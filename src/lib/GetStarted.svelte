18d01775773ec96534823079fc05f1990e039902
Oleh Hurmanchuk <gyrmanchyk230506@gmail.com>
2024-02-29 15:20:06 +0200

    add boost your links section

---
+++ src/lib/GetStarted.svelte
@@ -0,0 +1,36 @@
+<div>
+  <h3>Boost your links today</h3>
+  <a href="/">Get started</a>
+</div>
+
+
+<style>
+  div {
+    display: flex;
+    width: 100%;
+    height: 12rem;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-image: url("./images/bg-shorten-desktop.svg");
+    background-repeat: no-repeat;
+    background-size: 100%;
+    background-color: var(--dark-violet);
+  }
+
+  h3 {
+    color: var(--white);
+    font-size: 1.5rem;
+    margin-top: 0;
+  }
+
+  a {
+    color: var(--white);
+    background-color: var(--cyan);
+    width: 8rem;
+    height: 2.5rem;
+    border-radius: 1.25rem;
+    line-height: 2.5rem;
+    text-align: center;
+  }
+</style>
--- src/lib/Main.svelte
+++ src/lib/Main.svelte
@@ -1,8 +1,13 @@
+<script>
+    import GetStarted from "./GetStarted.svelte";
+
+</script>
 <main>
   Main content
   Lorem ipsum dolor sit amet consectetur adipisicing elit. Est explicabo quasi nisi illo temporibus! Expedita, vel debitis! Cupiditate error unde iusto vitae nemo deleniti eum rem? Dolorem repudiandae neque eius.
   Molestiae sunt voluptates amet aliquam omnis aperiam sed minima, quae animi adipisci, eligendi vel iusto sint minus quibusdam impedit. Consectetur sint ad aspernatur consequuntur tenetur odio nam nobis nihil quasi.
   Quaerat voluptate at sapiente enim, voluptates neque animi aut. Atque, repellat laboriosam fugit labore nemo voluptates perferendis earum repudiandae saepe possimus corporis molestias. Quae corporis numquam commodi suscipit deleniti itaque.
+  <GetStarted />
 </main>
 
 <style>
